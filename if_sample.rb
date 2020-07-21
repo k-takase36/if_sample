78697721ab24b16419e8eed8c725fcf696854370
takaseko <takaseko@takasekouheinoMacBook-Pro.local>
2020-07-21 15:35:03 +0900

    if_簡単な復習

---
+++ if_sample.rb
@@ -0,0 +1,25 @@
+# num = 13
+
+# if num > 10
+#   puts "10より大きいです"
+# end
+
+# p num > 10
+# p num < 10
+# p num >= 10
+# p num <= 10
+# p num == 10
+
+num = 13
+
+# if num >= 10
+#   puts "10以上です"
+# elsif num >= 5
+#   puts "5以上です"
+# elsif num >= 3
+#   puts "3以上です"
+# else
+#   puts "2以下です"
+# end
+
+puts "10以上です" if num >= 10
