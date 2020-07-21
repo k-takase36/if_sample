6261c02248fe9636e7e1eab8cdd25e0baf3b1a24
takaseko <takaseko@takasekouheinoMacBook-Pro.local>
2020-07-21 17:10:59 +0900

    戦闘プログラム作成

--- conditional.rb
+++ conditional.rb
@@ -11,15 +11,40 @@ enemy_attack = 5
 # 敵の防御力
 enemy_defense = 10
 
-# 敵に与えるダメージの計算
-enemy_damage = brave_attack - enemy_defense
+# # 敵に与えるダメージの計算
+# enemy_damage = brave_attack - enemy_defense
+# # 敵のHPにダメージを与える
+# enemy_hp = enemy_hp - enemy_damage
 
-# 敵のHPにダメージを与える
-enemy_hp = enemy_hp - enemy_damage
+# 攻撃にランダム要素を入れる1
+# 範囲オブジェクト 3..5
+# enemy_damage = brave_attack - enemy_defense + rand(3..5)
+# enemy_hp = enemy_hp - enemy_damage
 
-# 敵に与えるダメージと残りHPの表示
-puts "敵に#{enemy_damage}のダメージを与えた！"
-puts "残りHPは#{enemy_hp}だ！"
+# 攻撃にランダム要素を入れる2
+# rand(4)にすると0~3のうちランダムに数字を発生させる
+select_attack = rand(4)
+
+if select_attack == 0
+  puts "会心の一撃"
+  # randの範囲を20~30と大きな値にする
+  enemy_damage = brave_attack - enemy_defense + rand(15..25)
+  enemy_hp = enemy_hp - enemy_damage
+else
+  puts "通常攻撃"
+  enemy_damage = brave_attack - enemy_defense + rand(3..5)
+  enemy_hp = enemy_hp - enemy_damage
+end
+
+# # 敵に与えるダメージと残りHPの表示
+# puts "敵に#{enemy_damage}のダメージを与えた！"
+# puts "残りHPは#{enemy_hp}だ！"
+
+# 上記だと長文のため、ヒアドキュメントとしてリファクタリング
+puts <<~TEXT
+敵に#{enemy_damage}のダメージを与えた。
+残りHPは#{enemy_hp}だ。
+TEXT
 
 # 残りHPによってリアクションを変える
 if enemy_hp > 20
@@ -34,15 +59,40 @@ else
   puts "敵は死んだ"
 end
 
-# 勇者が受けるダメージの計算
-brave_damage = enemy_attack - brave_defense
+# # 勇者が受けるダメージの計算
+# brave_damage = enemy_attack - brave_defense
+# # 勇者が受けるHPの計算
+# brave_hp = brave_hp - brave_damage
 
-# 勇者が受けるHPの計算
-brave_hp = brave_hp - brave_damage
+# 攻撃にランダム要素を入れる1
+# 範囲オブジェクト 3..5
+# brave_damage = enemy_attack - brave_defense + rand(3..5)
+# brave_hp = brave_hp - brave_damage
+
+# 攻撃にランダム要素を入れる2
+# rand(4)にすると0~3のうちランダムに数字を発生させる
+select_attack = rand(4)
+
+if select_attack == 0
+  puts "会心の一撃"
+  # randの範囲を20~30と大きな値にする
+  brave_damage = enemy_attack - brave_defense + rand(5..15)
+  brave_hp = brave_hp - brave_damage
+else
+  puts "通常攻撃"
+  brave_damage = enemy_attack - brave_defense + rand(3..5)
+  brave_hp = brave_hp - brave_damage
+end
 
 # 勇者が受けるダメージと残りのHPの表示
-puts "敵から#{brave_damage}のダメージを受けた！"
-puts "残りのHPは#{brave_hp}だ！"
+# puts "敵から#{brave_damage}のダメージを受けた！"
+# puts "残りのHPは#{brave_hp}だ！"
+
+# 上記だと長文のため、ヒアドキュメントとしてリファクタリング
+puts <<~TEXT
+敵から#{brave_damage}のダメージを受けた。
+残りHPは#{brave_hp}だ。
+TEXT
 
 if brave_hp > 20
   puts "勇者は元気だ"
