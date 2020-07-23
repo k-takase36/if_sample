7fbd3b7340d64f45e79eb76c07b8cbfab8f91ce6
takaseko <takaseko@takasekouheinoMacBook-Pro.local>
2020-07-23 10:57:23 +0900

    商品選択、商品個数入力時の適用外エラーの処理追加

--- if_greengrocer_task.rb
+++ if_greengrocer_task.rb
@@ -17,7 +17,8 @@ end
 while true 
   print "商品を選択 >"
   vegetable_product = gets.chomp.to_i
-  break 
+  break if(1..4).include?(vegetable_product)
+    puts "不正な値です。1〜4の値を入力お願いします。"
 end
 
 selected_vegetable = vegetables[vegetable_product -1] 
@@ -28,18 +29,17 @@ puts "#{selected_vegetable[:name]}ですね、何個買いますか？"
 while true 
   print "個数を入力 >"
   vegetable_num = gets.chomp.to_i
-  break
+  break if vegetable_num > 0
+    puts "八百屋で野菜何も買わないで帰るんですか！笑。再度1以上を入力し購入お願いします！"
 end
 
 # 合計金額の表示（割引含む）
-  total_price = vegetable_num * selected_vegetable[:price]
-  discounted_total_price = (total_price * 0.8).round
+total_price = vegetable_num * selected_vegetable[:price]
+discounted_total_price = (total_price * 0.8).round
 
-  if vegetable_num <= 0
-    puts "0は不正な値で購入できません。再度1以上を入力お願いします。"
-  elsif vegetable_num < 5
-    puts "合計料金：¥#{total_price}"
-  else 
-    puts "5個以上なので20%割引となります。"
-    puts "合計料金：¥#{discounted_total_price}"
-  end
+if vegetable_num < 5
+  puts "合計料金：¥#{total_price}"
+else 
+  puts "5個以上なので20%割引となります。"
+  puts "合計料金：¥#{discounted_total_price}"
+end
